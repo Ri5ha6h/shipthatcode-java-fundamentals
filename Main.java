2533ebf54882aa663e512606bdca265040549b6b
Ri5ha6h <workwithmalik97@gmail.com>
2026-08-21 16:22:05 +0530

    done

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Print the greeting below.
-        System.out.println("Hello, Java!");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
     }
 }
