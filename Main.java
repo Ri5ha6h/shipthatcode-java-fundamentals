17e2dddbb32fbbc8ebb3023dae6de8e194a27d6d
Ri5ha6h <workwithmalik97@gmail.com>
2026-08-21 17:51:27 +0530

    done

--- Main.java
+++ Main.java
@@ -2,14 +2,20 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int square(int n) {
-        return n * n;
+    record Point(int x1, int y1, int x2, int y2) {
+        int distance() {
+            return (x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1);
+        }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
 
-        System.out.println(square(num));
+        Point newPoint = new Point(x1, y1, x2, y2);
+        System.out.println(newPoint.distance());
     }
 }
