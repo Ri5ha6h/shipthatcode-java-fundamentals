f6af3942f018a5dfb5d878d373102b76bb4c8e4f
Ri5ha6h <workwithmalik97@gmail.com>
2026-08-21 16:26:55 +0530

    done

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
 
-        System.out.println(a + b);
+        System.out.println(w * h);
     }
 }
