d2d207c37af6c2ac93ec1085b58ceee704d7b5f7
Ri5ha6h <workwithmalik97@gmail.com>
2026-08-21 16:34:03 +0530

    done

--- Main.java
+++ Main.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int w = sc.nextInt();
-        int h = sc.nextInt();
+        String line = sc.nextLine();
 
-        System.out.println(w * h);
+        System.out.println(line.toUpperCase());
     }
 }
