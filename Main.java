6e23d03876cd7e08202d28f2e8570dd340f306b9
Ri5ha6h <workwithmalik97@gmail.com>
2026-08-21 17:59:39 +0530

    done

--- Main.java
+++ Main.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class Main {
 
-    record Point(int x1, int y1, int x2, int y2) {
-        int distance() {
-            return (x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1);
+    record Point(int x, int y) {
+        int distance(Point other) {
+            int dx = x - other.x;
+            int dy = y - other.y;
+            return (dx * dx + dy * dy);
         }
     }
 
@@ -15,7 +17,7 @@ public class Main {
         int x2 = sc.nextInt();
         int y2 = sc.nextInt();
 
-        Point newPoint = new Point(x1, y1, x2, y2);
-        System.out.println(newPoint.distance());
+        Point newPoint = new Point(x1, y1);
+        System.out.println(newPoint.distance(new Point(x2, y2)));
     }
 }
