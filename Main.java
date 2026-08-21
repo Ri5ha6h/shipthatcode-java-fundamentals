7deee73de5d9a1cfd4c3a32279a49ce516ffc2fc
Ri5ha6h <workwithmalik97@gmail.com>
2026-08-21 16:10:31 +0530

    done

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // Print the greeting below.
-        IO.println("Hello, Java!");
+        System.out.println("Hello, Java!");
     }
 }
