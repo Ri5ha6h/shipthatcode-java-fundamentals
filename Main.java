bf879adb23afa22ff28972f9442a51f341716b57
Ri5ha6h <workwithmalik97@gmail.com>
2026-08-21 16:47:48 +0530

    done

--- Main.java
+++ Main.java
@@ -3,9 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        int age = sc.nextInt();
+        int num = sc.nextInt();
 
-        System.out.printf("Hi, %s! You are %d years old.%n", name, age);
+        if(num % 15 == 0){
+            System.out.println("FizzBuzz");
+        }
+        else if(num % 3 == 0){
+            System.out.println("Fizz");
+        }
+        else if(num % 5 == 0){
+            System.out.println("Buzz");
+        }
+        else {
+            System.out.println(num);
+        }
     }
 }
