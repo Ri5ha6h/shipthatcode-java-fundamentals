dcaa6b51a8739775040ce169e98e70775b86c981
Ri5ha6h <workwithmalik97@gmail.com>
2026-08-24 19:48:48 +0530

    interfaces complete

--- Main.java
+++ Main.java
@@ -1,43 +1,21 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
+    static int safeDivide(int a, int b) {
+        try{
+            return a / b;
+        } catch(ArithmeticException e){
+            return -1;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String kind = sc.nextLine();
-        double dim = Double.parseDouble(sc.nextLine());
-        Shape s = kind.equals("circle") ? new Circle(dim) : new Square(dim);
-        System.out.println(String.format("%.2f", s.area()));
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(safeDivide(a, b));
     }
 }
 
-interface Shape {
-    double area();
-}
 
-class Circle implements Shape {
-    private  double radius;
-
-    Circle(double rad) {
-        this.radius = rad;
-    }
-
-    @Override
-    public double area() {
-        return Math.PI * radius * radius;
-    }
-}
-
-class Square implements Shape {
-    private  double side;
-
-    Square(double side) {
-        this.side = side;
-    }
-
-    @Override
-    public double area() {
-        return side * side;
-    }
-}
